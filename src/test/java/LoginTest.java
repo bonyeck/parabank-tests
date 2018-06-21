aab578faa98b278a0b74f5ba32ccf148ca7efbcc
tomasz bonior <tomasz.bonior@gmail.com>
2018-06-21 21:13:35 +0200

    clear imports

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,12 +1,7 @@
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.AccountPage;
-import pages.IndexPage;
 
 public class LoginTest {
 
