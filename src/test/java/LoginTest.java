68a8b6eba24f800d54bb34831d071374c314c9ca
tomasz bonior <tomasz.bonior@gmail.com>
2018-06-21 21:12:23 +0200

    clearing

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -11,23 +11,15 @@ import pages.IndexPage;
 public class LoginTest {
 
     WebDriver driver;
-    IndexPage indexPage;
-    AccountPage accountPage;
 
     @BeforeTest
     public void before() {
         driver = new ChromeDriver();
-        indexPage = new IndexPage(driver);
-        accountPage = new AccountPage(driver);
     }
 
     @Test
     public void shouldLogin() {
-        indexPage.openIndexPage();
-        indexPage.setLogin("John");
-        indexPage.setPassword("123123!$L");
-        indexPage.clickLoginButton();
-        Assert.assertTrue(accountPage.isUserLoggedIn());
+       //let's add some test
     }
 }
 
