ef1d858fe91ce467a065a51a089207ddde6a9c7b
tomasz bonior <tomasz.bonior@gmail.com>
2018-06-21 20:15:56 +0200

    LoginTest example

--- src/main/java/pages/IndexPage.java
+++ src/main/java/pages/IndexPage.java
@@ -3,28 +3,22 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class IndexPage extends BasePage {
+public class IndexPage {
+    private WebDriver driver;
 
     public IndexPage(WebDriver driver) {
-        super(driver);
+        this.driver=driver;
     }
-
-    public IndexPage openIndexPage() {
+    public void openIndexPage(){
         driver.get("http://parabank.parasoft.com");
-        waitForJStoLoad();
-        return this;
     }
-
-    public void setUsername(String username) {
-        driver.findElement(By.cssSelector("[name=username]")).sendKeys(username);
+    public void setLogin(String login) {
+        driver.findElement(By.cssSelector("[name=username]")).sendKeys(login);
     }
-
     public void setPassword(String password) {
         driver.findElement(By.cssSelector("[name=password]")).sendKeys(password);
     }
-
     public void clickLoginButton() {
         driver.findElement(By.cssSelector(".login .button")).click();
     }
-
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,30 +1,34 @@
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AccountPage;
+import pages.IndexPage;
 
-public class LoginTest extends BaseTest {
+public class LoginTest {
+
+    WebDriver driver;
+    IndexPage indexPage;
+    AccountPage accountPage;
 
     @BeforeTest
-    public void beforeTest(){
-        indexPage.openIndexPage();
-        indexPage.openRegistrationPage();
-        registerPage.setFirstName(firstName);
-        registerPage.setLastName(lastName);
-        registerPage.setStreet(street);
-        registerPage.setCity(city);
-        registerPage.setState(state);
-        registerPage.setZipCode(zipCode);
-        registerPage.setSsn(ssn);
-        registerPage.setUsername(username);
-        registerPage.setPassword(password);
-        registerPage.setRepeatedPassword(password);
-        registerPage.clickRegisterButton();
-        accountPage.logout();
+    public void before() {
+        driver = new ChromeDriver();
+        indexPage = new IndexPage(driver);
+        accountPage = new AccountPage(driver);
     }
 
     @Test
     public void shouldLogin() {
-        //let's add some test
+        indexPage.openIndexPage();
+        indexPage.setLogin("John");
+        indexPage.setPassword("123123!$L");
+        indexPage.clickLoginButton();
+        Assert.assertTrue(accountPage.isUserLoggedIn());
     }
-
 }
+
+
