848086bee05f6712e2de93217943c1eaee158d2a
tomasz bonior <tomasz.bonior@gmail.com>
2018-06-21 20:20:17 +0200

    PageFactory for IndexPage

--- src/main/java/pages/IndexPage.java
+++ src/main/java/pages/IndexPage.java
@@ -1,24 +1,39 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class IndexPage {
     private WebDriver driver;
 
+    @FindBy(css = "[name=username]")
+    private WebElement userNameInput;
+
+    @FindBy(css = "[name=username]")
+    private WebElement passwordInput;
+
+    @FindBy(css = "[name=username]")
+    private WebElement loginButton;
+
+
     public IndexPage(WebDriver driver) {
-        this.driver=driver;
+        this.driver = driver;
     }
-    public void openIndexPage(){
+
+    public void openIndexPage() {
         driver.get("http://parabank.parasoft.com");
     }
+
     public void setLogin(String login) {
-        driver.findElement(By.cssSelector("[name=username]")).sendKeys(login);
+        userNameInput.sendKeys(login);
     }
+
     public void setPassword(String password) {
-        driver.findElement(By.cssSelector("[name=password]")).sendKeys(password);
+        passwordInput.sendKeys(password);
     }
+
     public void clickLoginButton() {
-        driver.findElement(By.cssSelector(".login .button")).click();
+        loginButton.click();
     }
 }
